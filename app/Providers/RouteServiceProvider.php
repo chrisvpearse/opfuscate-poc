3fbc0b4f6b08bb1ae52f5f0cc56602eb270965cd
Christopher Pearse <88168658+chrisvpearse@users.noreply.github.com>
2024-01-17 03:32:40 +0700

    A fix to run console commands with zeroed out controllers

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
